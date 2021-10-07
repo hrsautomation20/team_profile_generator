3218dda1cb740f1bbe1ef71ff9b42dd68c6b49d7
Hardik Rola <hrsautomation20@gmail.com>
2021-10-07 18:10:58 -0400

    Updated intern test - PASSED

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,46 @@
 const Intern = require("../lib/Intern");
+
+describe("Intern", () => {
+  describe("intern object", () => {
+    it("should create an object with 'intern'", () => {
+      const obj = {
+        name: "Hardik",
+        id: 1111,
+        email: "hrsautomation20@gmail.com",
+        school: "University Of NorthCarolina",
+      };
+      const intern = new Intern(obj);
+      expect(intern.name).toEqual(expect.any(String));
+      expect(intern.id).toEqual(expect.any(Number));
+      expect(intern.email).toEqual(expect.any(String));
+      expect(intern.school).toEqual(expect.any(String));
+    });
+  });
+  describe("intern school", () => {
+    it("should get intern's school name", () => {
+      const obj = {
+        name: "Hardik",
+        id: 1111,
+        email: "hrsautomation20@gmail.com",
+        school: "University Of NorthCarolina",
+      };
+      const school = "University Of NorthCarolina";
+      const intern = new Intern(obj, school);
+      expect(intern.getSchool()).toEqual(expect.any(String));
+    });
+  });
+  describe("intern role", () => {
+    it("should get intern's role", () => {
+      const obj = {
+        name: "Hardik",
+        id: 1111,
+        email: "hrsautomation20@gmail.com",
+        school: "University Of NorthCarolina",
+        role: "Intern",
+      };
+      const gitHub = "hrsautomation20";
+      const intern = new Intern(obj, gitHub);
+      expect(intern.getRole()).toEqual(expect.any(String));
+    });
+  });
+});
