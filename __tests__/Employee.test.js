7d2bfdd319a61ebcacb40e00c1dc32d100f3edef
Hardik Rola <hrsautomation20@gmail.com>
2021-10-07 09:01:26 -0400

    Updated Employee test - PASSED

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,63 @@
 const Employee = require("../lib/Employee");
+
+describe("Employee", () => {
+  describe("employee object", () => {
+    it("should create an object with 'employee'", () => {
+      const obj = {
+        name: "Hardik",
+        id: 1111,
+        email: "hrsautomation20@gmail.com",
+      };
+      const employee = new Employee(obj);
+      expect(employee.name).toEqual(expect.any(String));
+      expect(employee.id).toEqual(expect.any(Number));
+      expect(employee.email).toEqual(expect.any(String));
+    });
+  });
+  describe("employee name", () => {
+    it("should get employee name", () => {
+      const obj = {
+        name: "Hardik",
+        id: 1111,
+        email: "hrsautomation20@gmail.com",
+      };
+      const employee = new Employee(obj);
+      expect(employee.getName()).toEqual(
+        expect.stringContaining(employee.name.toString())
+      );
+    });
+  });
+  describe("employee id", () => {
+    it("should get employee id", () => {
+      const obj = {
+        name: "Hardik",
+        id: 1111,
+        email: "hrsautomation20@gmail.com",
+      };
+      const employee = new Employee(obj);
+      expect(employee.getId()).toEqual(expect.any(Number));
+    });
+  });
+  describe("employee email", () => {
+    it("should get employee email", () => {
+      const obj = {
+        name: "Hardik",
+        id: 1111,
+        email: "hrsautomation20@gmail.com",
+      };
+      const employee = new Employee(obj);
+      expect(employee.getEmail()).toEqual(
+        expect.stringContaining(employee.email.toString())
+      );
+    });
+  });
+  describe("employee role", () => {
+    it("should get employee role", () => {
+      const obj = {
+        role: "Employee",
+      };
+      const employee = new Employee(obj);
+      expect(employee.getRole()).toEqual(expect.any(String));
+    });
+  });
+});
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,31 @@
 const Engineer = require("../lib/Engineer");
+
+describe("Engineer", () => {
+  describe("engineer object", () => {
+    it("should create an object with 'engineer'", () => {
+      const obj = {
+        name: "Hardik",
+        id: 1111,
+        email: "hrsautomation20@gmail.com",
+      };
+      const engineer = new Engineer(obj);
+      expect(engineer.name).toEqual(expect.any(String));
+      expect(engineer.id).toEqual(expect.any(Number));
+      expect(engineer.email).toEqual(expect.any(String));
+      expect(engineer.gitHub).toEqual(expect.any(String));
+    });
+  });
+  describe("github url", () => {
+    it("should get github URL", () => {
+      const engineer = new Engineer(this.obj);
+      expect(engineer.getGitHub()).toEqual(expect.any(String));
+    //   console.log(engineer.getGitHub());
+    });
+  });
+  describe("engineer role", () => {
+    it("should get engineer role", () => {
+      const engineer = new Engineer(this.obj);
+      expect(engineer.getRole()).toEqual(expect.any(String));
+    });
+  });
+});
